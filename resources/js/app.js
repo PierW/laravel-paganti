3d661e44cc119523a21f0063a5e2dc002b6fccbe
Pier.W <factory666@live.it>
2019-05-22 13:30:12 +0200

    added memo

--- resources/js/app.js
+++ resources/js/app.js
@@ -20,11 +20,16 @@ function getTime() {
 
 function init() {
 
+  //PER ESEGUIRE ALCUNE FUNZIONI SOLO IN ALCUNI ROOT POSSIAMO FARE COSI:
   // var newURL = window.location.protocol + "//" + window.location.host + window.location.pathname + window.location.search;
   var patch = window.location.pathname;
   if (patch === "/home") {
     console.log(newURL, patch);
   }
+
+  //OPPURE CREANDO UN ALTRO FILE (ES. VEDI PROVA.JS, E FILE WEBPACK RICHIAMATA SOLO IN ROOT /HOME --- VIEW/BOOLEAN
+  // MA IN QUESTO CASO DOBBIAMO INCLUDERE JQUERY PIù VOLTE ALTRIMENTI DA ERRORE IN COMPILAZIONE CON WEBPACK)
+
   // console.log("Hello World");
   getTime();
 }
